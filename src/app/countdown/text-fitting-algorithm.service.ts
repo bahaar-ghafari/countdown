30ffba0861934aa060ad32e0926f98157fc6bac7
bahaar-ghafari <zahraghafari1996@gmail.com>
2024-09-02 10:43:21 +0200

    refactor: use parameters

--- src/app/countdown/text-fitting-algorithm.service.ts
+++ src/app/countdown/text-fitting-algorithm.service.ts
@@ -9,15 +9,20 @@ export class TextFittingAlgorithmService {
   adjustFontSizeToFit(container: ElementRef) {
     const containerWidth = container.nativeElement.clientWidth;
     const element = container.nativeElement;
+    const defaultFontSize = 8;
+    const minimumFontSize = 0.2;
+    const maximumFontSize = 10;
 
-    element.style.fontSize = '5rem';
+    element.style.fontSize = `${defaultFontSize}rem`;
 
-    let textWidth = this.getTextWidth(element.innerText, '5rem');
+    // Ensure the text width is measured after the default size is applied
+    let textWidth = this.getTextWidth(element.innerText, `${defaultFontSize}rem`);
 
-    let ratio = (containerWidth / textWidth) * 0.85; 
+    // Calculate the ratio with a slightly reduced ratio to ensure it fits
+    let ratio = (containerWidth / textWidth);
 
-    let newFontSize = 5 * ratio; 
-    newFontSize = Math.min(Math.max(0.2, newFontSize), 10); 
+    let newFontSize = defaultFontSize * ratio;
+    newFontSize = Math.min(Math.max(minimumFontSize, newFontSize), maximumFontSize);
 
     element.style.fontSize = `${newFontSize}rem`;
 
